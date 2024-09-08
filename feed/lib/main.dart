860bddbf892828f47feb76c8c2537d879646402a
Henry Herrmann <henryherrmann.tv@gmail.com>
2024-09-08 20:21:19 +0200

    Delete all documents before saving new articles

--- feed/lib/main.dart
+++ feed/lib/main.dart
@@ -42,8 +42,30 @@ Future<dynamic> main(final context) async {
 
     if(data['news'] == null || data['news']!.length == 0) continue;
 
+    var documents;
+
+    try {
+      documents = await database.listDocuments(
+        databaseId: 'feed',
+        collectionId: locale,
+      );
+    } catch (e) {
+      context.error("[-] Unable to retrieve documents in collection $locale. Error: $e");
+    }
+
+    for(const doc in documents) {
+      try {
+        await database.deleteDocument(
+          databaseId: 'feed',
+          collectionId: locale,
+          documentId: doc.$id,
+        );
+      } catch (e) {
+        context.error("[-] Unable to delete document ${doc.$id}. Error: $e");
+      }
+    }
+
     for(final NewsEntity n in data['news']!) {
-      context.log(n.url);
       String encoded;
       try {
         encoded = jsonEncode(n.toInternalJson());
