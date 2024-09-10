27363adb14def1551076f29d8b4d867eaf665421
Henry Herrmann <henryherrmann.tv@gmail.com>
2024-09-10 17:45:00 +0200

    Fix bug

--- feed/lib/translate/libre_translate_requests.dart
+++ feed/lib/translate/libre_translate_requests.dart
@@ -22,7 +22,9 @@ Future<String> translateText(String text, String sourceLang, String targetLang, 
   if (response.statusCode == 200) {
     final jsonResponse = json.decode(utf8.decode(response.bodyBytes));
     return jsonResponse['translatedText'];
+  } else if (response.statusCode != 404 || response.statusCode != 500) {
+    context.log('Encountered error. Status Code: ${response.statusCode}, Body: ${response.body}');
   } else {
-    throw Exception('Status Code: ${response.statusCode}, Body: ${response.body}');
+    throw ServerException();
   }
 }
